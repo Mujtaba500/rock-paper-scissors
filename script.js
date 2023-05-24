c036969b4d40cff360d43d62d77ec28c3ea3c928
Mujtaba500 <126389386+Mujtaba500@users.noreply.github.com>
2023-05-24 15:41:11 +0500

    Added UserInterface to the game.

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 
-
+let result = document.querySelector('#result')
+let playerSels = document.querySelectorAll('.player-sel')
+let winCount = document.querySelector('#win-count')
+let lossCount = document.querySelector('#loss-count')
+Array.from(playerSels).forEach(element => element.addEventListener('click', game))
 let winCounter = 0
-let tieCounter = 0
+let lossCounter = 0
 
-function game(playerSelection, computerSelection){
-    // FOR LOOP used for 5 rounds
-    for( i = 0; i < 5; i++){
+function game(event){
+        if( winCounter >= 5){
+        result.textContent = "Congrats! You are the winner!";
+        return;
+        }
+        else if( lossCounter >= 5){
+            result.textContent = "You just lost to a machine! That's just sad";
+            return;
+        }
         function getComputerChoice(){  // randomly generated answer by computer
             let x = Math.floor(Math.random() * 3)
             if( x === 0){
@@ -19,54 +29,58 @@ function game(playerSelection, computerSelection){
             }
         }
         
-        let computerSelection = getComputerChoice()
-        let playerSelection = prompt('Rock Paper or Scissors?').toLowerCase()  
+         computerSelection = getComputerChoice()
+
+         if(event.target.classList.contains('rock')){
+            playerSelection = 'rock'
+         } else if(event.target.classList.contains('paper')){
+            playerSelection = 'paper'
+         } else{
+            playerSelection = 'scissors'
+         }
 
     if( computerSelection === "rock" && playerSelection === "scissors" ){
-        console.log(("You LOST! Rock beats Scissors" ))
+        result.textContent = "You LOST this round! Rock beats Scissors" 
+        lossCounter++
+        lossCount.textContent = `Computer score: ${lossCounter}`
+        
     }
     else if(computerSelection === "paper" && playerSelection === "rock"){
-        console.log(("You LOST! Paper beats Rock"))  
+        result.textContent =  "You LOST this round! Paper beats Rock"
+        lossCounter++
+        lossCount.textContent = `Computer score: ${lossCounter}`
     }
     else if(computerSelection === "scissors" && playerSelection === "paper"){
-        console.log(("You LOST! Scissors beat Paper"))
+        result.textContent = "You LOST this round! Scissors beat Paper"
+        lossCounter++
+        lossCount.textContent = `Computer score: ${lossCounter}`
     }
     else if(computerSelection === playerSelection){
-        console.log(("Its a TIE"))
-        tieCounter++
+        result.textContent = "Its a TIE"
     }
     else if(playerSelection === "rock" && computerSelection === "scissors"){
-        console.log("You Won this round! Rock beats Scissors")
+        result.textContent = "You Won this round! Rock beats Scissors"
         winCounter++
+        winCount.textContent = `Player score: ${winCounter}`
     }
     else if(playerSelection === "scissors" && computerSelection === "paper"){
-        console.log("You Won this round! Scissors beat paper")
+        result.textContent = "You Won this round! Scissors beat paper"
         winCounter++
+        winCount.textContent = `Player score: ${winCounter}`
     }
     else if(playerSelection === "paper" && computerSelection === "rock"){
-        console.log("You Won this round! Paper beats rock")
+        result.textContent = "You Won this round! Paper beats rock"
         winCounter++
+        winCount.textContent = `Player score: ${winCounter}`
     }
-    else {
-        console.log(("You lost because of incorrect spelling"))
-       
-    }
-}
-}
-
-//Call function
-game()
-
-let lossCounter = 5 - winCounter - tieCounter
-
-// Display the results after 5 rounds
-if( winCounter > 2){
-    alert(`You are the winner! You won ${winCounter} rounds out of 5`)
-    console.log(`Congrats! You won ${winCounter} rounds out of 5`)
-}
-else{
-    alert(`You LOST ${lossCounter} rounds to a machine!`)
-    console.log(`You LOST ${lossCounter} rounds to a machine!`)
+    if( winCounter >= 5){
+        result.textContent = "Congrats! You are the winner!";
+        return;
+        }
+    else if( lossCounter >= 5){
+            result.textContent = "You just lost to a machine! That's just sad";
+            return;
+        }
 }
 
 
